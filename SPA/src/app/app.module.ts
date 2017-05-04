7393af49d0828ad438024c443ef1b19518c15f2e
victor <victorpc05@gmail.com>
2017-05-04 18:15:42 -0500

    heroes

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quickstart/src/app/components/body.component.ts
+++ quickstart/src/app/components/body.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: 'body.component.html',
 })
 export class BodyComponent {
+  mostrar:boolean = false;
 
+  texto:string = "Un gran poder";
+  author:string = "Ben Parker";
+
+  personajes:string[] = ["Spiderman", "Venon", "Dr. Octopus"];
 }
